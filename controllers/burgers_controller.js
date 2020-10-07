ec51d9cbaafd50c6a9f055f99512c91776604c7c
dantalmont <dantalmont@gmail.com>
2020-10-07 00:53:44 -0400

    fixed typo

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -10,7 +10,7 @@ var burger = require('../models/burger.js');
 router.get('/', function(req, res) {
   burger.all(function(data) {
     var hbsObject = {
-      burgers: data
+      BURGERS: data
     };
     // console.log(hbsObject);
     res.render('index', hbsObject);
