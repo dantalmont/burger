7f1e32bd250ef45e04212949c257f3428bf0b4f2
dantalmont <dantalmont@gmail.com>
2020-10-07 00:12:30 -0400

    added key folders

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO BURGERS (burger_name, devoured) VALUES ("Krabby Patty", true);
+INSERT INTO BURGERS (burger_name, devoured) VALUES ("Smokehouse Burger", true);
+INSERT INTO BURGERS (burger_name, devoured) VALUES ("Baconator", true);
+
