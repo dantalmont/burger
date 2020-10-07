d5da99f164d1acaeef8afcf11f6bbfa1789e961e
dantalmont <dantalmont@gmail.com>
2020-10-07 00:14:02 -0400

    added additional folders for server

---
+++ public/assets/js/burger.js
@@ -0,0 +1,60 @@
+$(function() {
+    $(".change-eaten").on("click", function(event) {
+      var id = $(this).data("id");
+      var newDevour = $(this).data("newDevour");
+  
+      var newDevourState = {
+        devoured: newDevoured
+      };
+  
+      // Send the PUT request.
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: newDevourState
+      }).then(
+        function() {
+          console.log("Burger Changed to:", newDevoured);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      var newBurger = {
+        name: $("#burgerName").val().trim(),
+        devoured: $("[name=devoured]:checked").val().trim()
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("Added a new burger");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".delete-burger").on("click", function(event) {
+      var id = $(this).data("id");
+  
+      // Send the DELETE request.
+      $.ajax("/api/burgers/" + id, {
+        type: "DELETE"
+      }).then(
+        function() {
+          console.log("tossed burger", id);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  });
+  
